723e06b54d2cd0a09a0ef4b0a5b267dc5c67bf56
丁勇 <hzdingyong@corp.netease.com>
2015-04-24 10:48:03 +0800

    add test

---
+++ src/test/java/blog/AbstractUnitTest.java
@@ -0,0 +1,15 @@
+/*
+ * @(#) AbstractUnitText.java 2015年4月24日
+ * 
+ * Copyright 2010 NetEase.com, Inc. All rights reserved.
+ */
+package blog;
+
+/**
+ *
+ * @author hzdingyong
+ * @version 2015年4月24日
+ */
+public class AbstractUnitTest {
+
+}
